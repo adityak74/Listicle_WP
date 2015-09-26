dffa30e79e7fec643ef03f9aaecbe61c2df9fbcc
Aditya K G <akarnam37@gmail.com>
2015-09-26 15:41:14 +0530

    Gallery Added

---
+++ index.php
@@ -0,0 +1,591 @@
+﻿<!DOCTYPE html>
+<html lang="en" class="no-js" >
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+	<meta name="description" content="" />
+	<meta name="author" content="" />
+<!--[if IE]>
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<![endif]-->
+<title>LIGHT WAVE</title>
+<!-- BOOTSTRAP CORE CSS -->
+<link href="assets/css/bootstrap.css" rel="stylesheet" />
+<!-- ION ICONS STYLES -->
+<link href="assets/css/ionicons.css" rel="stylesheet" />
+<!-- FONT AWESOME ICONS STYLES -->
+<link href="assets/css/font-awesome.css" rel="stylesheet" />
+<!-- FANCYBOX POPUP STYLES -->
+<link href="assets/js/source/jquery.fancybox.css" rel="stylesheet" />
+<!-- STYLES FOR VIEWPORT ANIMATION -->
+<link href="assets/css/animations.min.css" rel="stylesheet" />
+<!-- CUSTOM CSS -->
+<link href="assets/css/style-green.css" rel="stylesheet" />
+<!-- HTML5 Shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+<body data-spy="scroll" data-target="#menu-section">
+	<!--MENU SECTION START-->
+	<div class="navbar navbar-inverse navbar-fixed-top scroll-me" id="menu-section" >
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">
+
+					LIGHT WAVE
+
+				</a>
+			</div>
+			<div class="navbar-collapse collapse">
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#home">HOME</a></li>
+					<li><a href="#contact">ABOUT US</a></li>
+					<li><a href="#work">WHERE WE WORK</a></li>
+					<li><a href="#grid">WHAT WE DO</a></li>
+					<li><a href="#services">GET INVOLVED</a></li>
+					<li><a href="#team">TEAM</a></li>
+					
+				</ul>
+			</div>
+
+		</div>
+	</div>
+	<!--MENU SECTION END-->
+	<!--HOME SECTION START-->
+	<div id="home" >
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2 ">
+					<div id="carousel-slider" data-ride="carousel" class="carousel slide  animate-in" data-anim-type="fade-in-up">
+
+						<div class="carousel-inner">
+							<div class="item active">
+
+								<h3>
+									Consectetur adipiscing elit felis dolor felis dolor vitae
+								</h3>
+								<p>
+									Lorem ipsumdolor sitamet, consect adipiscing elit
+									Lorem ipsumdolor sitamet, consect adipiscing elit.
+									Lorem ipsumdolor sitamet, consect adipiscing elit
+									Lorem ipsumdolor sitamet, consect adipiscing elit.
+								</p>
+							</div>
+							<div class="item">
+								<h3>
+									Lorem ipsumdolor sitamet, consect adipiscing elit
+								</h3>
+								<p>
+									Lorem ipsumdolor sitamet, consect adipiscing elit
+									Lorem ipsumdolor sitamet, consect adipiscing elit.
+									Lorem ipsumdolor sitamet, consect adipiscing elit
+									Lorem ipsumdolor sitamet, consect adipiscing elit.
+								</p>
+							</div>
+
+						</div>
+
+
+					</div>
+
+
+				</div>
+			</div>
+			<div class="row animate-in" data-anim-type="fade-in-up">
+				<div class="col-sm-6 col-sm-offset-3 col-md-6 col-md-offset-3 col-lg-8 col-lg-offset-2 scroll-me">
+
+
+					<p >
+						This line is fixed so you can write anything
+
+					</p>
+					<div class="social">
+						<a href="#" class="btn button-custom btn-custom-one" ><i class="fa fa-facebook "></i></a>
+						<a href="#" class="btn button-custom btn-custom-one" ><i class="fa fa-twitter"></i></a>
+						<a href="#" class="btn button-custom btn-custom-one" ><i class="fa fa-google-plus "></i></a>
+						<a href="#" class="btn button-custom btn-custom-one" ><i class="fa fa-linkedin "></i></a>
+						<a href="#" class="btn button-custom btn-custom-one" ><i class="fa fa-pinterest "></i></a>
+						<a href="#" class="btn button-custom btn-custom-one" ><i class="fa fa-github "></i></a>
+					</div>
+					<a href="#services" class=" btn button-custom btn-custom-two">See Service List </a>
+				</div>
+			</div>
+		</div>
+
+	</div>
+	<!--HOME SECTION END-->
+	
+	<!--CONTACT SECTION START-->
+	<section id="contact" >
+		<div class="container">
+			<div class="row text-center header animate-in" data-anim-type="fade-in-up">
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+
+					<h3>About Us </h3>
+					<hr />
+
+				</div>
+			</div>
+
+			<div class="row animate-in" data-anim-type="fade-in-up">
+
+				<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+					<div class="contact-wrapper">
+						<h3>History</h3>
+						<p>
+							Aliquam tempus ante placerat,
+							consectetur tellus nec, porttitor nulla.
+						</p>
+					</div>
+
+				</div>
+				<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+					<div class="contact-wrapper">
+						<h3>Aim</h3>
+						<h4><strong>Email : </strong> info@yuordomain.com </h4>
+						<h4><strong>Call : </strong> +09-88-99-77-55 </h4>
+						<h4><strong>Skype : </strong> Yujhaeu78 </h4>
+					</div>
+
+				</div>
+				<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+					<div class="contact-wrapper">
+						<h3>Vision </h3>
+						<h4>230/45 , New way Lane , </h4>
+						<h4>United States</h4>
+						<div class="footer-div" >
+							&copy; 2015 YourDomain | <a href="http://www.designbootstrap.com/" target="_blank" >by DesignBootstrp</a>
+						</div>
+					</div>
+
+				</div>
+				<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+					<div class="contact-wrapper">
+						<h3>Organistaion</h3>
+						<p>
+							Aliquam tempus ante placerat,
+							consectetur tellus nec, porttitor nulla.
+						</p>
+					</div>
+
+				</div>
+				<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+					<div class="contact-wrapper">
+						<h3>Working For Aasya Foundation</h3>
+						<p>
+							Aliquam tempus ante placerat,
+							consectetur tellus nec, porttitor nulla.
+						</p>
+						<div class="social-below">
+							<a href="#" class="btn button-custom btn-custom-two" > Intern With Us</a>
+							<a href="#" class="btn button-custom btn-custom-two" > Volunteer</a>
+						</div>
+					</div>
+
+				</div>
+				
+
+			</div>
+
+
+		</div>
+	</section>
+	<!--CONTACT SECTION END-->
+
+	<!--WORK SECTION START-->
+	<section id="work" >
+		<div class="container">
+			<div class="row text-center header animate-in" data-anim-type="fade-in-up">
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+					<h3>WHERE WE WORK</h3>
+					<hr />
+				</div>
+			</div>
+			<div class="row text-center animate-in" data-anim-type="fade-in-up" >
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 pad-bottom">
+					<div class="caegories">
+						<?php
+							$eventTitleArray = array('Heart Patient Kids',
+							 'Malaria Awarness',
+							 'Food At Old Age Home',
+							 'Food For Orphans(FFO)',
+							 'Each 1 Teach 1(e1t1)',
+							 'Food At Old Age Home(FAOAH)',
+							 'Food For Orphans(FFO)',
+							 'Mega Health Camp(MHC)',
+							 'Flash Mob'
+							);
+							$evnt = "evnt";
+							$events=1;
+							foreach ($eventTitleArray as $title) {
+								# code...
+							
+						?>
+							<a href="#" data-filter=".<?php echo $evnt.$events; ?>" style="margin:5px" class="btn btn-custom btn-custom-two btn-sm"><?php echo $title; ?></a>
+						<?php
+							$events++;
+							}
+						?>
+					</div>
+				</div>
+			</div>
+			<div class="row text-center animate-in" data-anim-type="fade-in-up" id="work-div">
+						<?php
+							$evnt = "evnt";
+							$ctr=0;
+							$directory = "assets/images/";
+							foreach (glob($directory . "*") as $filename) {
+								$ctr++;
+							} 
+
+
+							for($i=1;$i<=$ctr;$i++){
+
+								$directory = 'assets/images/' . $evnt . $i . '/';
+								$files = glob($directory . '*.jpg',GLOB_NOSORT);
+
+								if ( $files !== false )
+								{
+									//echo $eventTitleArray[$i-1] . "<br>";
+									$filecount = count( $files );
+									//echo $filecount . "<br>";
+									$j=0;
+									while($j<=11 && $j<$filecount){
+										$imageLink = "assets/images/evnt" . $i . "/" . ($j+1) .  ".jpg";
+										$j++;
+										echo "<div class='col-xs-12 col-sm-4 col-md-4 col-lg-4 ". $evnt.$i ."'>
+												<div class='work-wrapper'>
+
+													<a class='fancybox-media' title='Image Title Goes Here' href='".$imageLink."'>
+
+														<img src='".$imageLink."' class='img-responsive img-rounded' alt='' />
+													</a>
+
+													<h4>Morbi mollis lectus et ipsum</h4>
+												</div>
+											</div>";
+									}
+
+								}
+								else
+								{
+									//echo 0;
+								}
+							}
+						?>
+				
+			</div>
+		</div>
+	</section>
+	<!--WORK SECTION END-->
+	<!--GRID SECTION START-->
+	<section id="grid" >
+		<div class="container">
+			<div class="row text-center header animate-in" data-anim-type="fade-in-up">
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+
+					<h3>WHAT WE DO</h3>
+					<hr />
+
+				</div>
+			</div>
+			<div class="row pad-bottom animate-in" data-anim-type="fade-in-up">
+
+				<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+					<h2>Disease Awarenes</h2>
+					<p>Content About Disease Awareness
+					</p>
+
+				</div>
+				<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+					<h2>Adult Literacy</h2>
+					<p>Content about Adult Literacy
+					</p>
+
+				</div>
+				<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+					<h2>Citizen Advocacy</h2>
+					<p>Content about Citizen Advocacy 
+					</p>
+
+				</div>
+				<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+					<h2>Blood Donation</h2>
+					<p>Content about Blood Donation 
+					</p>
+
+				</div>
+			</div>
+			<div class="row pad-bottom animate-in" data-anim-type="fade-in-up">
+
+				<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+					<h2>Affordable Medical Testing</h2>
+					<p>Content About this</p>
+
+				</div>
+				<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+					<h2>Research and Development</h2>
+					<p>Content About this.
+					</p>
+
+				</div>
+				<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+					<h2>Child and Women Protection</h2>
+					<p>Content About this.
+					</p>
+
+				</div>
+
+			</div>
+			<div class="row pad-bottom animate-in" data-anim-type="fade-in-up">
+
+				<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+					<h2>Organ Donation Pledge Drives</h2>
+					<p>Content About Organ Donation</p>
+
+				</div>
+				<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+					<h2>Food Nourishment Activities</h2>
+					<p>Content About Nourishment 
+					</p>
+
+				</div>
+
+
+			</div>
+
+			<div class="row pad-bottom animate-in" data-anim-type="fade-in-up">
+
+				<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+					<h2>Rehab of acid, rape, accident victims</h2>
+					<p>Content About this</p>
+
+				</div>
+				<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+					<h2>Pet adoption and rescue</h2>
+					<p>Content About this 
+					</p>
+
+				</div>
+
+
+			</div>
+
+			<div class="row pad-bottom animate-in" data-anim-type="fade-in-up">
+
+				<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+					<h2>Legal Clinic</h2>
+					<p>Content About this.
+					</p>
+
+				</div>
+				<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+					<h2>Awareness Campaigns</h2>
+					<p>Content about.
+					</p>
+
+				</div>
+				<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+					<h2>De-Addiction</h2>
+					<p>Content about this. 
+					</p>
+
+				</div>
+				<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+					<h2>Agricultural Testing</h2>
+					<p>Content about this. 
+					</p>
+
+				</div>
+			</div>
+
+
+
+		</div>
+	</section>
+	<!--GRID SECTION END-->
+
+	<!--SERVICE SECTION START-->
+	<section id="services" >
+		<div class="container">
+			<div class="row text-center header">
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 animate-in" data-anim-type="fade-in-up">
+					<h3>GET INVOLVED</h3>
+					<hr />
+				</div>
+			</div>
+			<div class="row animate-in" data-anim-type="fade-in-up">
+				<a href="#" style="color:white">
+					<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+						<div class="services-wrapper">
+							<i class="ion-document"></i>
+							<h3>Where does your money go</h3>
+							Morbi mollis lectus et ipsum sollicitudin varius.
+							Aliquam tempus ante placerat, consectetur tellus nec, porttitor nulla.
+						</div>
+					</div>
+				</a>
+				<a href="#" style="color:white">
+					<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+						<div class="services-wrapper">
+							<i class="ion-scissors"></i>
+							<h3>Partnering with corporations</h3>
+							Morbi mollis lectus et ipsum sollicitudin varius.
+							Aliquam tempus ante placerat, consectetur tellus nec, porttitor nulla.
+						</div>
+					</div>
+				</a>
+				<a href="#" style="color:white">
+					<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+						<div class="services-wrapper">
+							<i class="ion-clipboard"></i>
+							<h3>Partnering with your Foundation or Trust</h3>
+							Morbi mollis lectus et ipsum sollicitudin varius.
+							Aliquam tempus ante placerat, consectetur tellus nec, porttitor nulla.
+						</div>
+					</div>
+				</a>
+				<a href="#" style="color:white">
+					<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+						<div class="services-wrapper">
+							<i class="ion-calendar"></i>
+							<h3>Bequest and Legacies</h3>
+							Morbi mollis lectus et ipsum sollicitudin varius.
+							Aliquam tempus ante placerat, consectetur tellus nec, porttitor nulla.
+						</div>
+					</div>
+				</a>
+				<a href="#" style="color:white">
+					<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+						<div class="services-wrapper">
+							<i class="ion-erlenmeyer-flask"></i>
+							<h3>Philanthropy</h3>
+							Morbi mollis lectus et ipsum sollicitudin varius.
+							Aliquam tempus ante placerat, consectetur tellus nec, porttitor nulla.
+						</div>
+					</div>
+				</a>
+				<a href="#" style="color:white">
+					<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+						<div class="services-wrapper">
+							<i class="ion-monitor"></i>
+							<h3>Shop</h3>
+							Morbi mollis lectus et ipsum sollicitudin varius.
+							Aliquam tempus ante placerat, consectetur tellus nec, porttitor nulla.
+						</div>
+					</div>
+				</a>
+
+
+			</div>
+		</div>
+	</section>
+	<!--SERVICE SECTION END-->
+
+	<!--TEAM SECTION START-->
+	<section id="team" >
+		<div class="container">
+			<div class="row text-center header animate-in" data-anim-type="fade-in-up">
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+					<h3>Team Members </h3>
+					<hr />
+				</div>
+			</div>
+			<div class="row animate-in" data-anim-type="fade-in-up">
+
+				<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+					<div class="team-wrapper">
+						<div class="team-inner" style="background-image: url('assets/img/team/1.jpg')" >
+							<a href="#" target="_blank" > <i class="fa fa-twitter" ></i></a>
+						</div>
+						<div class="description">
+							<h3> Leomarid Jona</h3>
+							<h5> <strong> Developer & Designer </strong></h5>
+							<p>
+								Pellentesque elementum dapibus convallis.
+								Vivamus eget finibus massa.
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+					<div class="team-wrapper">
+						<div class="team-inner" style="background-image: url('assets/img/team/2.jpg')" >
+							<a href="#" target="_blank" > <i class="fa fa-twitter" ></i></a>
+						</div>
+						<div class="description">
+							<h3> Leomarid Jona</h3>
+							<h5> <strong> Developer & Designer </strong></h5>
+							<p>
+								Pellentesque elementum dapibus convallis.
+								Vivamus eget finibus massa.
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+					<div class="team-wrapper">
+						<div class="team-inner" style="background-image: url('assets/img/team/3.jpg')" >
+							<a href="#" target="_blank" > <i class="fa fa-twitter" ></i></a>
+						</div>
+						<div class="description">
+							<h3> Leomarid Jona</h3>
+							<h5> <strong> Developer & Designer </strong></h5>
+							<p>
+								Pellentesque elementum dapibus convallis.
+								Vivamus eget finibus massa.
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+					<div class="team-wrapper">
+						<div class="team-inner" style="background-image: url('assets/img/team/4.jpg')" >
+							<a href="#" target="_blank" > <i class="fa fa-twitter" ></i></a>
+						</div>
+						<div class="description">
+							<h3> Leomarid Jona</h3>
+							<h5> <strong> Developer & Designer </strong></h5>
+							<p>
+								Pellentesque elementum dapibus convallis.
+								Vivamus eget finibus massa.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--TEAM SECTION END-->
+	
+
+	
+	<!-- JAVASCRIPT FILES PLACED AT THE BOTTOM TO REDUCE THE LOADING TIME -->
+	<!-- CORE JQUERY -->
+	<script src="assets/js/jquery-1.11.1.js"></script>
+	<!-- BOOTSTRAP SCRIPTS -->
+	<script src="assets/js/bootstrap.js"></script>
+	<!-- EASING SCROLL SCRIPTS PLUGIN -->
+	<script src="assets/js/vegas/jquery.vegas.min.js"></script>
+	<!-- VEGAS SLIDESHOW SCRIPTS -->
+	<script src="assets/js/jquery.easing.min.js"></script>
+	<!-- FANCYBOX PLUGIN -->
+	<script src="assets/js/source/jquery.fancybox.js"></script>
+	<!-- ISOTOPE SCRIPTS -->
+	<script src="assets/js/jquery.isotope.js"></script>
+	<!-- VIEWPORT ANIMATION SCRIPTS   -->
+	<script src="assets/js/appear.min.js"></script>
+	<script src="assets/js/animations.min.js"></script>
+	<!-- CUSTOM SCRIPTS -->
+	<script src="assets/js/custom.js"></script>
+</body>
+
+</html>
