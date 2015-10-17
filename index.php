990936a4cd35a72963cb9a4f6d8bd96a03b4bcba
Aditya K G <akarnam37@gmail.com>
2015-10-17 19:55:37 +0530

    Added Meta Keywords

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <head>
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-	<meta name="description" content="" />
-	<meta name="author" content="" />
+	<meta name="description" content="Aasya Health Foundation" />
+	<meta name="author" content="Aasya Health Foundation" />
 <!--[if IE]>
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <![endif]-->
